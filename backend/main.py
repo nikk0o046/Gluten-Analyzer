96c7d961720e2955523162fb66a4e8912d7d7a01
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-10-04 10:22:54 +0300

    restrict backend CORS origins

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # should be restricted in production
+    allow_origins=["https://nikk0o046.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
